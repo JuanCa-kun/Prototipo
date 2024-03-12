ef4dff0e517cf68026a61f800c966433ed9fe84c
Juan Carlos Hernández Montero <juancarlos.hdz.montero@gmail.com>
2024-03-12 11:56:29 -0600

    Nuevo ejercicio

---
+++ Ejercicio8.py
@@ -0,0 +1,10 @@
+palabra = input("Ingrese la oración que desea invertir: ")
+string = []
+cadenaInvertida = ""
+
+for inver in palabra:
+    string.append(inver)
+
+for i in range(len(palabra) - 1, 0, -1):
+    cadenaInvertida += string[i]
+print(f"Está es la oración: {cadenaInvertida}")
